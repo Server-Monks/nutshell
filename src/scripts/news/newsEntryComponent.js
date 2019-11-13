e77d79b5b091bb4ddef24109f6e60f0f9e3bfee2
Corri Golden <corri.golden@gmail.com>
2019-11-13 11:28:18 -0600

    added add article format and factory function

--- src/scripts/news/newsEntryComponent.js
+++ src/scripts/news/newsEntryComponent.js
@@ -1,4 +1,24 @@
 
 
 
+const entryManager = {
+
+    makeJournalEntryComponent(articleEntry) {
+        // Create your own HTML structure for a journal entry
+        return `
+        <section id=journalEntries>
+          <h3>${articleEntry.title}</h3>
+          <p>${articleEntry.description}</p>
+        </section>
+       
+      `
+    },  // created a factory function for the object of creating a journal.
+    createArticleObj(date, concept, entry, mood) {
+        return {
+            "title": title,
+            "description": description,
+        }
+    }
+}
+
 
