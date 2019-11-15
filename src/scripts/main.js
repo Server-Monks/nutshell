6ee519af0bf4ddca5c40299b7d5a0df28738c218
Caroline Brownlee <brownleecaroline@gmail.com>
2019-11-15 14:47:53 -0600

    ready to start coding login and registration, but need to create a new branch. Oops.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -46,16 +46,13 @@ doThaThang.registerDeleteListener()
 doThaThang.registerEditListener()
 })
 
+// import initRegistration from "./registration/mainRegistration";
+// import dashboardFunctions from "./dashboard";
 
-
-
-
-
-
-
-
-
-   
-    
-
-  
+// let activeUserId = sessionStorage.getItem("activeUser")
+// if (activeUserId === null) {
+//     initRegistration()
+// } else {
+//     // Render initial display
+//     dashboardFunctions()
+// }
